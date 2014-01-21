fe5d69ded4433f68b61b42988255eaf56a9bb015
Rutvik Sheth <shethru@ip-172-31-37-129.us-west-2.compute.internal>
2014-01-21 15:33:29 +0000

    first push bmkfundmeas.txt reader

---
+++ test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+from cassandra.cluster import Cluster
+import csv
+#cluster=Cluster(['localhost'])
+#session=cluster.connect()
+
+
+#if cluster.describeKeyspace("mykeyspace")==null:
+# print "exists"
+
+with 
